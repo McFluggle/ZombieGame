50d447f63d5eb80e9c0d94e515477ad707308e09
McFluggle <60100673+McFluggle@users.noreply.github.com>
2020-02-13 23:34:52 +0000

    Add files via upload

---
+++ Cell.java
@@ -0,0 +1,45 @@
+public class Cell
+{
+  private Terrain terrain;
+  private Entity entity;
+
+  public Cell(Terrain _T, Entity _E)
+  {
+    terrain = _T;
+    entity = _E;
+  }
+
+  /*OnLeftClick
+  OnRightClick*/
+
+  //SetEntity
+  //GetEntity
+
+  //GetTerrian
+}
+
+class Terrain
+{
+  private int variant;
+  private int type;
+
+  public Terrain(int _V, int _T)
+  {
+    variant = _V;
+    type = _T;
+  }
+
+  //GetTerrian
+  //GetVariant
+}
+
+class Entity
+{
+  private int health;
+  private int armour;
+}
+
+class Zombie extends Entity
+{
+  private int type;
+}
