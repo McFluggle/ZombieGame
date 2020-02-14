22de3669fc2cfaf4d973d90b0c70346b2f16c6bd
Joe Garrett <joe.garrett@warwick.ac.uk>
2020-02-14 00:00:37 +0000

    Added ability to deal damage to entities, and made constructors and get/set better

--- Cell.java
+++ Cell.java
@@ -5,17 +5,26 @@ public class Cell
 
   public Cell(Terrain _T, Entity _E)
   {
-    terrain = _T;
-    entity = _E;
+    this.terrain = _T;
+    this.entity = _E;
   }
 
   /*OnLeftClick
   OnRightClick*/
 
-  //SetEntity
-  //GetEntity
+  public void setEntity(Entity _E)
+  {
+    this.entity = _E;
+  }
+  public Entity getEntity()
+  {
+    return this.entity;
+  }
 
-  //GetTerrian
+  public Terrain getTerrain()
+  {
+    return this.terrain;
+  }
 }
 
 class Terrain
@@ -25,23 +34,51 @@ class Terrain
 
   public Terrain(int _V, int _T)
   {
-    variant = _V;
-    type = _T;
+    this.variant = _V;
+    this.type = _T;
   }
 
-  //GetTerrian
-  //GetVariant
+  public int getVariant()
+  {
+    return this.variant;
+  }
+  public int getType()
+  {
+    return this.type;
+  }
 }
 
 class Entity
 {
   private int health;
   private int armour;
+
+  public Entity(int _H, int _A)
+  {
+    this.health = _H;
+    this.armour = _A;
+  }
+
+  public void dealDamage(int damageDealt, boolean piercing)
+  {
+    int damageToArmour = min(damageDealt, Armour);
+    int damageToHealth = damageDealt - damageToArmour;
+
+    this.armour = this.armour - damageToArmour;
+
+    if (damageToArmour == 0 || piercing)
+    {
+      this.health = this.health - damageToHealth;
+    }
+
+    if (this.health == 0)
+    {
+      //Kill
+    }
+  }
 }
 
 class Zombie extends Entity
 {
   private int type;
 }
-
-//Hello!!
